db404315a8cad1f5d9e6280d729b1f395282088d
Iwona Winiarska <iwona.winiarska@intel.com>
2022-12-19 22:06:59 +0100

    i3c: target: Add ops to read dynamic address
    In case HW doesn't notify that a target's dynamic address was changed
    (e.g. due to RSTDAA command), we can't rely on the dynamic address
    information in I3C device info struct.
    Let's extend target operations with a method that allows reading
    a target's dynamic address in-place.
    
    Signed-off-by: Iwona Winiarska <iwona.winiarska@intel.com>
    Change-Id: I3a774024edc3c9c87bbd2e307fb3cd82023f5e37

--- include/linux/i3c/target.h
+++ include/linux/i3c/target.h
@@ -14,6 +14,7 @@ struct i3c_target_ops {
 	void (*bus_cleanup)(struct i3c_master_controller *master);
 	int (*priv_xfers)(struct i3c_dev_desc *dev, struct i3c_priv_xfer *xfers, int nxfers);
 	int (*generate_ibi)(struct i3c_dev_desc *dev, const u8 *data, int len);
+	u8 (*get_dyn_addr)(struct i3c_master_controller *master);
 };
 
 int i3c_target_register(struct i3c_master_controller *master, struct device *parent,
