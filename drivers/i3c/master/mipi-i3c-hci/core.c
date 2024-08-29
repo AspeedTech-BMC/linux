f821fab0478fc085e72f64ba74acbe42d552d507
Shyam Sundar S K <Shyam-sundar.S-k@amd.com>
2024-08-29 14:47:09 +0530

    i3c: mipi-i3c-hci: Read HC_CONTROL_PIO_MODE only after i3c hci v1.1
    The HC_CONTROL_PIO_MODE bit was introduced in the HC_CONTROL register
    starting from version 1.1. Therefore, checking the HC_CONTROL_PIO_MODE bit
    on hardware that adheres to older specification revisions (i.e., versions
    earlier than 1.1) is incorrect. To address this, add an additional check
    to read the HCI version before attempting to read the HC_CONTROL_PIO_MODE
    status.
    
    Signed-off-by: Shyam Sundar S K <Shyam-sundar.S-k@amd.com>
    Reviewed-by: Jarkko Nikula <jarkko.nikula@linux.intel.com>
    Link: https://lore.kernel.org/r/20240829091713.736217-3-Shyam-sundar.S-k@amd.com
    Signed-off-by: Alexandre Belloni <alexandre.belloni@bootlin.com>
    Change-Id: I0633f3e2edc15a0043e1fe9a7d00a6033e0599ce

--- drivers/i3c/master/mipi-i3c-hci/core.c
+++ drivers/i3c/master/mipi-i3c-hci/core.c
@@ -1316,8 +1316,8 @@ static irqreturn_t i3c_aspeed_irq_handler(int irqn, void *dev_id)
 
 static int i3c_hci_init(struct i3c_hci *hci)
 {
+	bool size_in_dwords, mode_selector;
 	u32 regval, offset;
-	bool size_in_dwords;
 	int ret;
 
 	/* Validate HCI hardware version */
@@ -1443,10 +1443,13 @@ static int i3c_hci_init(struct i3c_hci *hci)
 		return -EINVAL;
 	}
 
+	mode_selector = hci->version_major > 1 ||
+				(hci->version_major == 1 && hci->version_minor > 0);
+
 	/* Try activating DMA operations first */
 	if (hci->RHS_regs) {
 		reg_clear(HC_CONTROL, HC_CONTROL_PIO_MODE);
-		if (reg_read(HC_CONTROL) & HC_CONTROL_PIO_MODE) {
+		if (mode_selector && (reg_read(HC_CONTROL) & HC_CONTROL_PIO_MODE)) {
 			dev_err(&hci->master.dev, "PIO mode is stuck\n");
 			ret = -EIO;
 		} else if (!hci->dma_rst) {
@@ -1468,7 +1471,7 @@ static int i3c_hci_init(struct i3c_hci *hci)
 	/* If no DMA, try PIO */
 	if (!hci->io && hci->PIO_regs) {
 		reg_set(HC_CONTROL, HC_CONTROL_PIO_MODE);
-		if (!(reg_read(HC_CONTROL) & HC_CONTROL_PIO_MODE)) {
+		if (mode_selector && !(reg_read(HC_CONTROL) & HC_CONTROL_PIO_MODE)) {
 			dev_err(&hci->master.dev, "DMA mode is stuck\n");
 			ret = -EIO;
 		} else {
