1eda31cf92fa3981b8feca2b47cc8ec1234f962e
Tommy Huang <tommy_huang@aspeedtech.com>
2025-03-07 14:42:14 +0800

    drivers: i2c: Seprarte the some slave handling cases
    Separate the Match+RX_done+Stop case. And remove the
    setting slave event stop from just slave match case
    
    Signed-off-by: Tommy Huang <tommy_huang@aspeedtech.com>
    Change-Id: Ia9bab3e5ee72c62847302e6bc41007397e3ae053

--- drivers/i2c/busses/i2c-ast2600.c
+++ drivers/i2c/busses/i2c-ast2600.c
@@ -254,6 +254,9 @@
 #define MSIC_CONFIG_ACTIMING1		0x74
 #define MSIC_I2C_SET_TIMEOUT(s, m)		(((s) << 16) | (m))
 
+/* 0x78 : Controller status */
+#define MSIC_STATUS		0x78
+
 /* 0x8c : Slave sirq log */
 #define AST2700_I2CC_SIRQ_LOG			0x8c
 #define SLAVE_ADDR_MASK			GENMASK(15, 8)
@@ -471,6 +474,8 @@ static void ast2700_i2c_slave_packet_dma_irq(struct ast2600_i2c_bus *i2c_bus, u3
 	}
 
 	if (AST2600_I2CS_ABNOR_STOP & sts) {
+		dev_err(i2c_bus->dev, "ABR stop slave isr case %x, sts %x\n", sts,
+			readl(i2c_bus->reg_base + MSIC_STATUS));
 		cmd = SLAVE_TRIGGER_CMD | AST2600_I2CS_RX_DMA_EN;
 		writel(AST2600_I2CS_SET_RX_DMA_LEN(I2C_SLAVE_MSG_BUF_SIZE),
 		       i2c_bus->reg_base + AST2600_I2CS_DMA_LEN);
@@ -490,6 +495,7 @@ static void ast2700_i2c_slave_packet_dma_irq(struct ast2600_i2c_bus *i2c_bus, u3
 		AST2600_I2CS_SLAVE_MATCH | AST2600_I2CS_RX_DONE | AST2600_I2CS_STOP:
 		writel(AST2600_I2CS_SLAVE_MATCH, i2c_bus->reg_base + AST2600_I2CS_ISR);
 		isr = readl(i2c_bus->reg_base + AST2600_I2CS_ISR);
+
 		sirq_log = readl(i2c_bus->reg_base + AST2700_I2CC_SIRQ_LOG);
 		i2c_slave_event(i2c_bus->slave, I2C_SLAVE_WRITE_REQUESTED,
 				&i2c_bus->slave_dma_buf[0]);
@@ -501,6 +507,7 @@ static void ast2700_i2c_slave_packet_dma_irq(struct ast2600_i2c_bus *i2c_bus, u3
 					&i2c_bus->slave_dma_buf[i]);
 		}
 		i2c_slave_event(i2c_bus->slave, I2C_SLAVE_STOP, &value);
+
 		sirq_log = readl(i2c_bus->reg_base + AST2700_I2CC_SIRQ_LOG);
 		i2c_slave_event(i2c_bus->slave, I2C_SLAVE_WRITE_REQUESTED,
 				&i2c_bus->slave_dma_buf[0]);
@@ -508,17 +515,48 @@ static void ast2700_i2c_slave_packet_dma_irq(struct ast2600_i2c_bus *i2c_bus, u3
 		       i2c_bus->reg_base + AST2600_I2CS_DMA_LEN);
 		cmd = SLAVE_TRIGGER_CMD | AST2600_I2CS_RX_DMA_EN;
 		break;
-	case AST2600_I2CS_SADDR_PENDING | AST2600_I2CS_SLAVE_MATCH |
-		AST2600_I2CS_RX_DONE | AST2600_I2CS_STOP:
 	case AST2600_I2CS_SLAVE_MATCH | AST2600_I2CS_RX_DONE | AST2600_I2CS_STOP:
-	case AST2600_I2CS_SLAVE_MATCH | AST2600_I2CS_STOP:
-	case AST2600_I2CS_SLAVE_MATCH:
 		sirq_log = readl(i2c_bus->reg_base + AST2700_I2CC_SIRQ_LOG);
+		/* bug workaround */
 		if (sirq_log & SADDR_HIT) {
 			i2c_slave_event(i2c_bus->slave, I2C_SLAVE_WRITE_REQUESTED,
 					&i2c_bus->slave_dma_buf[0]);
 			sirq_log = readl(i2c_bus->reg_base + AST2700_I2CC_SIRQ_LOG);
 		}
+		slave_rx_len = AST2600_I2C_GET_RX_DMA_LEN(readl(i2c_bus->reg_base +
+				      AST2600_I2CS_DMA_LEN_STS));
+		dev_dbg(i2c_bus->dev, "D-%d %x P\n", slave_rx_len, sirq_log);	//sirq == 0x15, pkt_done | rx-done | P
+		for (i = 0; i < slave_rx_len; i++) {
+			i2c_slave_event(i2c_bus->slave, I2C_SLAVE_WRITE_RECEIVED,
+					&i2c_bus->slave_dma_buf[i]);
+		}
+		writel(AST2600_I2CS_SET_RX_DMA_LEN(I2C_SLAVE_MSG_BUF_SIZE),
+		       i2c_bus->reg_base + AST2600_I2CS_DMA_LEN);
+		i2c_slave_event(i2c_bus->slave, I2C_SLAVE_STOP, &value);
+		cmd = SLAVE_TRIGGER_CMD | AST2600_I2CS_RX_DMA_EN;
+		break;
+	case AST2600_I2CS_SLAVE_MATCH:
+		sirq_log = readl(i2c_bus->reg_base + AST2700_I2CC_SIRQ_LOG);
+		i2c_slave_event(i2c_bus->slave, I2C_SLAVE_WRITE_REQUESTED,
+				&i2c_bus->slave_dma_buf[0]);
+		sirq_log = readl(i2c_bus->reg_base + AST2700_I2CC_SIRQ_LOG);
+		slave_rx_len = AST2600_I2C_GET_RX_DMA_LEN(readl(i2c_bus->reg_base +
+							  AST2600_I2CS_DMA_LEN_STS));
+		for (i = 0; i < slave_rx_len; i++) {
+			i2c_slave_event(i2c_bus->slave, I2C_SLAVE_WRITE_RECEIVED,
+					&i2c_bus->slave_dma_buf[i]);
+		}
+		writel(AST2600_I2CS_SET_RX_DMA_LEN(I2C_SLAVE_MSG_BUF_SIZE),
+		       i2c_bus->reg_base + AST2600_I2CS_DMA_LEN);
+		cmd = SLAVE_TRIGGER_CMD | AST2600_I2CS_RX_DMA_EN;
+		break;
+	case AST2600_I2CS_SADDR_PENDING | AST2600_I2CS_SLAVE_MATCH |
+		AST2600_I2CS_RX_DONE | AST2600_I2CS_STOP:
+	case AST2600_I2CS_SLAVE_MATCH | AST2600_I2CS_STOP:
+		sirq_log = readl(i2c_bus->reg_base + AST2700_I2CC_SIRQ_LOG);
+		i2c_slave_event(i2c_bus->slave, I2C_SLAVE_WRITE_REQUESTED,
+				&i2c_bus->slave_dma_buf[0]);
+		sirq_log = readl(i2c_bus->reg_base + AST2700_I2CC_SIRQ_LOG);
 		slave_rx_len = AST2600_I2C_GET_RX_DMA_LEN(readl(i2c_bus->reg_base +
 							  AST2600_I2CS_DMA_LEN_STS));
 		for (i = 0; i < slave_rx_len; i++) {
