62780ff63b7c2cda8bd5958d8ca369778ee0e50a
aspeedyh <yh_chung@aspeedtech.com>
2025-06-26 18:07:13 +0800

    soc: mctp: Fix memory allocation API usage
    use devm_kasprintf instead of kasprintf for dynamic memory allocation
    in aspeed_mctp_probe.
    
    The difference is that devm_kasprintf automatically frees the memory
    automatically when the device is removed, preventing memory leaks.
    While kasprintf requires manual memory management, which can lead to
    memory leaks if not handled properly.
    
    Signed-off-by: aspeedyh <yh_chung@aspeedtech.com>
    Change-Id: Iacc1c73aff9b2c9f8f99695c9439e23c8756734f

--- drivers/soc/aspeed/aspeed-mctp.c
+++ drivers/soc/aspeed/aspeed-mctp.c
@@ -2165,6 +2165,7 @@ static void aspeed_mctp_hw_reset(struct aspeed_mctp *priv)
 static int aspeed_mctp_probe(struct platform_device *pdev)
 {
 	struct aspeed_mctp *priv;
+	char *name;
 	int ret, id;
 	u16 bdf;
 
@@ -2224,7 +2225,7 @@ static int aspeed_mctp_probe(struct platform_device *pdev)
 		return id;
 	priv->mctp_miscdev.parent = priv->dev;
 	priv->mctp_miscdev.minor = MISC_DYNAMIC_MINOR;
-	priv->mctp_miscdev.name = kasprintf(GFP_KERNEL, "aspeed-mctp%d", id);
+	priv->mctp_miscdev.name = devm_kasprintf(priv->dev, GFP_KERNEL, "aspeed-mctp%d", id);
 	priv->mctp_miscdev.fops = &aspeed_mctp_fops;
 	ret = misc_register(&priv->mctp_miscdev);
 	if (ret) {
@@ -2256,8 +2257,9 @@ static int aspeed_mctp_probe(struct platform_device *pdev)
 		aspeed_mctp_rx_trigger(&priv->rx);
 	}
 
-	priv->peci_mctp = platform_device_register_data(
-		priv->dev, "peci-mctp", PLATFORM_DEVID_AUTO, NULL, 0);
+	name = devm_kasprintf(priv->dev, GFP_KERNEL, "peci-mctp%d", id);
+	priv->peci_mctp =
+		platform_device_register_data(priv->dev, name, PLATFORM_DEVID_NONE, NULL, 0);
 	if (IS_ERR(priv->peci_mctp))
 		dev_err(priv->dev, "Failed to register peci-mctp device\n");
 
