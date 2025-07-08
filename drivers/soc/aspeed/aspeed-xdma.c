0887ed7d00cd83a45a99ec77aafb1dc7e19417f6
Jammy Huang <jammy_huang@aspeedtech.com>
2025-07-08 11:57:46 +0800

    soc: aspeed: xdma: Avoid PCIE packet error
    PCIE's max payload size could be less than current's xdma setting, 256
    btyes. If it is 128 bytes, pcie's packet could mess up.
    
    Set it to min value, 128 btyes, to avoid this issue.
    
    Signed-off-by: Jammy Huang <jammy_huang@aspeedtech.com>
    Change-Id: If12dc2635c0b5b05c1d7aa6438a731c2c994b4e3

--- drivers/soc/aspeed/aspeed-xdma.c
+++ drivers/soc/aspeed/aspeed-xdma.c
@@ -1352,7 +1352,7 @@ static const struct aspeed_xdma_chip aspeed_ast2600_xdma_chip = {
 
 static const struct aspeed_xdma_chip aspeed_ast2700_xdma0_chip = {
 	.control = XDMA_AST2700_CTRL_US_COMP | XDMA_AST2700_CTRL_DS_COMP |
-		XDMA_AST2700_CTRL_DS_DIRTY | XDMA_AST2700_CTRL_DS_SIZE_256,
+		XDMA_AST2700_CTRL_DS_DIRTY,
 	.scu_bmc_class = SCU_AST2700_PCIE0_BMC_CLASS_REV,
 	.scu_misc_ctrl = SCU_AST2600_MISC_CTRL,
 	.scu_pcie_conf = SCU_AST2700_PCIE0_CONF,
@@ -1377,7 +1377,7 @@ static const struct aspeed_xdma_chip aspeed_ast2700_xdma0_chip = {
 
 static const struct aspeed_xdma_chip aspeed_ast2700_xdma1_chip = {
 	.control = XDMA_AST2700_CTRL_US_COMP | XDMA_AST2700_CTRL_DS_COMP |
-		XDMA_AST2700_CTRL_DS_DIRTY | XDMA_AST2700_CTRL_DS_SIZE_256,
+		XDMA_AST2700_CTRL_DS_DIRTY,
 	.scu_bmc_class = SCU_AST2700_PCIE1_BMC_CLASS_REV,
 	.scu_misc_ctrl = SCU_AST2600_MISC_CTRL,
 	.scu_pcie_conf = SCU_AST2700_PCIE1_CONF,
