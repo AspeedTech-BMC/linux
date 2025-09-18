46c9b51d7e771d3741ac5d5fa13151fbb0fd3780
Cool Lee <cool_lee@aspeedtech.com>
2025-09-18 09:51:31 +0800

    dt-bindings: ast2600-clock: Add sdio reset
    Signed-off-by: Cool Lee <cool_lee@aspeedtech.com>
    Change-Id: I09d0c7532c234502df998af9b15828dd483cb301

--- include/dt-bindings/clock/ast2600-clock.h
+++ include/dt-bindings/clock/ast2600-clock.h
@@ -93,6 +93,7 @@
 
 /* Only list resets here that are not part of a gate */
 #define ASPEED_RESET_ESPI		57
+#define ASPEED_RESET_SDIO		56
 #define ASPEED_RESET_ADC		55
 #define ASPEED_RESET_JTAG_MASTER2	54
 #define ASPEED_RESET_MAC4		53
