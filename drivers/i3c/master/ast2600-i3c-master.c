3c9821cb9812d38c9dff51114decbd095f445a42
Billy Tsai <billy_tsai@aspeedtech.com>
2025-12-08 11:24:59 +0800

    i3c: ast2600: handle null transfers during response processing
    Fixes a null transfer scenario by adding error handling for responses
    when no transfer is active. Logs detailed errors for invalid responses
    and exits halt state for critical errors to avoid potential deadlocks.
    This condition has appeared when SDA experienced multiple glitches, and
    the root cause has not been identified at this time. Therefore, the patch
    is used to mitigate this uncertain issue and prevent an interrupt storm
    caused by the response queue not draining in the IRQ handler.
    
    Signed-off-by: Billy Tsai <billy_tsai@aspeedtech.com>
    Change-Id: Iab698991c21bf5a3f508c92f8f204f2b800a57b2

--- drivers/i3c/master/ast2600-i3c-master.c
+++ drivers/i3c/master/ast2600-i3c-master.c
@@ -1134,11 +1134,28 @@ static void aspeed_i3c_master_end_xfer_locked(struct aspeed_i3c_master *master, 
 	int i, ret = 0;
 	u32 nresp;
 
-	if (!xfer)
-		return;
-
 	nresp = readl(master->regs + QUEUE_STATUS_LEVEL);
 	nresp = QUEUE_STATUS_LEVEL_RESP(nresp);
+
+	if (!xfer) {
+		u32 err_resp;
+		u8 error;
+
+		dev_err(&master->base.dev,
+			"Handle %d response when xfer is NULL\n", nresp);
+		for (i = 0; i < nresp; i++) {
+			err_resp = readl(master->regs + RESPONSE_QUEUE_PORT);
+			error = RESPONSE_PORT_ERR_STATUS(err_resp);
+			dev_err(&master->base.dev,
+				"err_resp: %x xfer error: %x\n", err_resp,
+				error);
+			if (error != RESPONSE_NO_ERROR &&
+			    error != RESPONSE_ERROR_TRANSF_ABORT) {
+				aspeed_i3c_master_resume(master);
+			}
+		}
+		return;
+	}
 
 	for (i = 0; i < nresp; i++) {
 		struct aspeed_i3c_cmd *cmd;
