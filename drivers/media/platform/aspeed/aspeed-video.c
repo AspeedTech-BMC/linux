03fd97ba416c1835c307c965244242a1d24e4c52
Jammy Huang <jammy_huang@aspeedtech.com>
2025-12-12 09:24:17 +0800

    media: aspeed: Fix VGA on/off NG in HP
    HP has their own KVM SW which does not close fd when KVM off. Adopt
    start/stop_streaming rather than fiel open/close to be compatible.
    
    Signed-off-by: Jammy Huang <jammy_huang@aspeedtech.com>
    Change-Id: Ia9f4519ce0894172fe4c7e184ca1fa597ad41cf6

--- drivers/media/platform/aspeed/aspeed-video.c
+++ drivers/media/platform/aspeed/aspeed-video.c
@@ -1828,8 +1828,6 @@ static void aspeed_video_init_regs(struct aspeed_video *video)
 
 static void aspeed_video_start(struct aspeed_video *video)
 {
-	aspeed_video_set_vga_on(video, true);
-
 	aspeed_video_on(video);
 
 	aspeed_video_init_regs(video);
@@ -1843,8 +1841,6 @@ static void aspeed_video_start(struct aspeed_video *video)
 
 static void aspeed_video_stop(struct aspeed_video *video)
 {
-	aspeed_video_set_vga_on(video, false);
-
 	set_bit(VIDEO_STOPPED, &video->flags);
 	cancel_delayed_work_sync(&video->res_work);
 
@@ -2516,6 +2512,8 @@ static int aspeed_video_start_streaming(struct vb2_queue *q,
 	int rc;
 	struct aspeed_video *video = vb2_get_drv_priv(q);
 
+	aspeed_video_set_vga_on(video, true);
+
 	video->sequence = 0;
 	video->perf.duration_max = 0;
 	video->perf.duration_min = 0xffffffff;
@@ -2540,6 +2538,8 @@ static void aspeed_video_stop_streaming(struct vb2_queue *q)
 {
 	int rc;
 	struct aspeed_video *video = vb2_get_drv_priv(q);
+
+	aspeed_video_set_vga_on(video, false);
 
 	clear_bit(VIDEO_STREAMING, &video->flags);
 
