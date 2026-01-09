b51ed83e5badde21954511577fd4c5d6605906dc
Tommy Huang <tommy_huang@aspeedtech.com>
2026-01-09 11:16:48 +0800

    i2c: ast2600: replace ENOMEM with EBUSY
    Since the error situation is related to share buffer condition.
    Replace the ENOMEM by EBUSY.
    
    Signed-off-by: Tommy Huang <tommy_huang@aspeedtech.com>
    Change-Id: Ib4b4c21924cbd6f31141b2e09baff5e54f5240d5

--- drivers/i2c/busses/i2c-ast2600.c
+++ drivers/i2c/busses/i2c-ast2600.c
@@ -1530,7 +1530,7 @@ static int ast2600_i2c_setup_buff_tx(u32 cmd, struct ast2600_i2c_bus *i2c_bus)
 		 * and write the remaining unaligned data at the end.
 		 */
 		if (readl(i2c_bus->reg_base + AST2600_I2CS_ISR))
-			return -ENOMEM;
+			return -EBUSY;
 		for (i = 0; i < xfer_len; i += 4) {
 			int xfer_cnt = i2c_bus->controller_xfer_cnt + i;
 
@@ -1551,13 +1551,13 @@ static int ast2600_i2c_setup_buff_tx(u32 cmd, struct ast2600_i2c_bus *i2c_bus)
 			writel(wbuf_dword, i2c_bus->buf_base + i);
 		}
 		if (readl(i2c_bus->reg_base + AST2600_I2CS_ISR))
-			return -ENOMEM;
+			return -EBUSY;
 		writel(AST2600_I2CC_SET_TX_BUF_LEN(xfer_len),
 		       i2c_bus->reg_base + AST2600_I2CC_BUFF_CTRL);
 	}
 
 	if (readl(i2c_bus->reg_base + AST2600_I2CS_ISR))
-		return -ENOMEM;
+		return -EBUSY;
 
 	writel(cmd, i2c_bus->reg_base + AST2600_I2CM_CMD_STS);
 
@@ -1747,7 +1747,7 @@ static void ast2600_i2c_controller_package_irq(struct ast2600_i2c_bus *i2c_bus, 
 		i2c_bus->msgs_index++;
 		if (i2c_bus->msgs_index < i2c_bus->msgs_count) {
 			if (ast2600_i2c_do_start(i2c_bus)) {
-				i2c_bus->cmd_err = -ENOMEM;
+				i2c_bus->cmd_err = -EBUSY;
 				complete(&i2c_bus->cmd_complete);
 			}
 		} else {
@@ -1776,7 +1776,7 @@ static void ast2600_i2c_controller_package_irq(struct ast2600_i2c_bus *i2c_bus, 
 				complete(&i2c_bus->cmd_complete);
 			} else {
 				if (ast2600_i2c_do_start(i2c_bus)) {
-					i2c_bus->cmd_err = -ENOMEM;
+					i2c_bus->cmd_err = -EBUSY;
 					complete(&i2c_bus->cmd_complete);
 				}
 			}
@@ -1853,7 +1853,7 @@ static void ast2600_i2c_controller_package_irq(struct ast2600_i2c_bus *i2c_bus, 
 				complete(&i2c_bus->cmd_complete);
 			} else {
 				if (ast2600_i2c_do_start(i2c_bus)) {
-					i2c_bus->cmd_err = -ENOMEM;
+					i2c_bus->cmd_err = -EBUSY;
 					complete(&i2c_bus->cmd_complete);
 				}
 			}
