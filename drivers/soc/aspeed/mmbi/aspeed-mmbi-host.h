a749b98520a6b454a400324398766fe7ff8b2b32
aspeedyh <yh_chung@aspeedtech.com>
2026-02-10 20:47:43 +0800

    soc: aspeed: add Host role support for MMBI
    add initial support for Host role in MMBI driver, including basic desc
    management and state transition. This is a prerequisite for implementing
     data transfer and other advanced features.
    
    Signed-off-by: aspeedyh <yh_chung@aspeedtech.com>
    Change-Id: I3b0bbd3ccc78683479204e9d3829e26e4efbe9b1

---
+++ drivers/soc/aspeed/mmbi/aspeed-mmbi-host.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0+ */
+/*
+ * Copyright 2026 Aspeed Technology Inc.
+ */
+#ifndef __ASPEED_MMBI_HOST_H__
+#define __ASPEED_MMBI_HOST_H__
+
+#include "aspeed-mmbi.h"
+
+int mmbi_instance_init_host(struct mmbi_ins_desc *mmbi);
+void mmbi_instance_irq_host(struct mmbi_ins_desc *mmbi);
+
+#endif /* __ASPEED_MMBI_HOST_H__ */
