62b385d1ae1e97a8068432b21457e01c8f6e4153
Ryan Chen <ryan_chen@aspeedtech.com>
2026-03-02 14:43:58 +0800

    dt-bindings: add sdio reset
    add sdhio reset
    
    Signed-off-by: Ryan Chen <ryan_chen@aspeedtech.com>
    Change-Id: I9245ec44fcc6cb70a158538d424f3c67f646ad07

--- include/dt-bindings/clock/ast2600-clock.h
+++ include/dt-bindings/clock/ast2600-clock.h
@@ -93,6 +93,7 @@
 
 /* Only list resets here that are not part of a clock gate + reset pair */
 #define ASPEED_RESET_ESPI		57
+#define ASPEED_RESET_SDIO		56
 #define ASPEED_RESET_ADC		55
 #define ASPEED_RESET_JTAG_MASTER2	54
 
