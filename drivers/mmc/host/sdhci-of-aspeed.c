32f4d97a0fffe5ca073de082324f08602f44e906
Cool Lee <cool_lee@aspeedtech.com>
2026-04-10 15:37:51 +0800

    mmc: sdhci-of-aspeed: Improve CMD6 timing
    The timing is critical before select_hs200 due to a 1_8V_SIGNAL is set.
    When this is set, the post edge between CLK and CMD is tight. Make an
    inverse to get a tolerance before CMD6 to switch HS200.
    
    Signed-off-by: Cool Lee <cool_lee@aspeedtech.com>
    Change-Id: Ia0486ea649299761fffe9c2f4132d4a5e369d841
    (cherry picked from commit e3532140fbe86fe88abacde37aebbbd5520fbc9d)

--- drivers/mmc/host/sdhci-of-aspeed.c
+++ drivers/mmc/host/sdhci-of-aspeed.c
@@ -326,6 +326,24 @@ static int aspeed_sdhci_execute_tuning(struct sdhci_host *host, u32 opcode)
 
 static void aspeed_sdhci_voltage_switch(struct sdhci_host *host)
 {
+	struct sdhci_pltfm_host *pltfm_priv;
+	struct aspeed_sdhci *sdhci;
+	struct aspeed_sdc *sdc;
+	u32 out_phase, enable_mask, inverted = 1;
+
+	pltfm_priv = sdhci_priv(host);
+	sdhci = sdhci_pltfm_priv(pltfm_priv);
+	sdc = sdhci->parent;
+
+	enable_mask = ASPEED_SDC_S0_PHASE_OUT_EN | ASPEED_SDC_S0_PHASE_IN_EN;
+
+	inverted = ASPEED_SDHCI_TAP_PARAM_INVERT_CLK;
+	out_phase = readl(sdc->regs + ASPEED_SDC_PHASE) & ASPEED_SDC_S0_PHASE_OUT;
+	out_phase |= (inverted << ASPEED_SDC_S0_PHASE_OUT_SHIFT);
+
+	/* output inverse */
+	writel(out_phase | enable_mask, sdc->regs + ASPEED_SDC_PHASE);
+
 	mdelay(30);
 }
 
