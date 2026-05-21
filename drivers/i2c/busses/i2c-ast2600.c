d0883d370f11c80f194ff6a55533eeaa7a20a485
Tommy Huang <tommy_huang@aspeedtech.com>
2026-05-21 19:12:57 +0800

    i2c: i2c-ast2600: add zero-length read quirk
    Add an i2c adapter quirk for zero-length read transfers.
    Remove the unnessary RX length check.
    
    Signed-off-by: Tommy Huang <tommy_huang@aspeedtech.com>
    Change-Id: I801fdce519968bfbecbd2f9eb42fe19d38bf62f3
    (cherry picked from commit 28c25f62ba714ee2212700ec9f119b79a1545e74)

--- drivers/i2c/busses/i2c-ast2600.c
+++ drivers/i2c/busses/i2c-ast2600.c
@@ -2176,9 +2176,6 @@ static int ast2600_i2c_setup_dma_rx(u32 cmd, struct ast2600_i2c_bus *i2c_bus)
 		cmd |= CONTROLLER_TRIGGER_LAST_STOP;
 	}
 
-	if (!xfer_len)
-		return -EINVAL;
-
 	writel(AST2600_I2CM_SET_RX_DMA_LEN(xfer_len - 1), i2c_bus->reg_base + AST2600_I2CM_DMA_LEN);
 
 	if (cmd & AST2600_I2CM_START_CMD)
@@ -2208,9 +2205,6 @@ static int ast2700_i2c_setup_buff_rx(u32 cmd, struct ast2600_i2c_bus *i2c_bus)
 		cmd |= CONTROLLER_TRIGGER_LAST_STOP;
 	}
 
-	if (!xfer_len)
-		return -EINVAL;
-
 	writel(AST2600_I2CM_SET_RX_DMA_LEN(xfer_len - 1),
 	       i2c_bus->reg_base + AST2600_I2CM_DMA_LEN);
 	writel(cmd, i2c_bus->reg_base + AST2600_I2CM_CMD_STS);
@@ -2237,9 +2231,6 @@ static int ast2600_i2c_setup_buff_rx(u32 cmd, struct ast2600_i2c_bus *i2c_bus)
 		cmd |= CONTROLLER_TRIGGER_LAST_STOP;
 	}
 
-	if (!xfer_len)
-		return -EINVAL;
-
 	writel(AST2600_I2CC_SET_RX_BUF_LEN(xfer_len), i2c_bus->reg_base + AST2600_I2CC_BUFF_CTRL);
 
 	writel(cmd, i2c_bus->reg_base + AST2600_I2CM_CMD_STS);
@@ -2250,7 +2241,6 @@ static int ast2600_i2c_setup_buff_rx(u32 cmd, struct ast2600_i2c_bus *i2c_bus)
 static int ast2600_i2c_setup_byte_rx(u32 cmd, struct ast2600_i2c_bus *i2c_bus)
 {
 	struct i2c_msg *msg = &i2c_bus->msgs[i2c_bus->msgs_index];
-	int xfer_len = msg->len;
 
 	cmd |= AST2600_I2CM_PKT_EN | AST2600_I2CM_RX_CMD;
 
@@ -2263,9 +2253,6 @@ static int ast2600_i2c_setup_byte_rx(u32 cmd, struct ast2600_i2c_bus *i2c_bus)
 		   ((i2c_bus->controller_xfer_cnt + 1) == msg->len)) {
 		cmd |= CONTROLLER_TRIGGER_LAST_STOP;
 	}
-
-	if (!xfer_len)
-		return -EINVAL;
 
 	writel(cmd, i2c_bus->reg_base + AST2600_I2CM_CMD_STS);
 
@@ -2872,6 +2859,10 @@ static const struct i2c_algorithm i2c_ast2600_algorithm = {
 #endif
 };
 
+static const struct i2c_adapter_quirks ast2600_i2c_quirks = {
+	.flags = I2C_AQ_NO_ZERO_LEN_READ,
+};
+
 static int ast2600_i2c_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -3053,6 +3044,7 @@ static int ast2600_i2c_probe(struct platform_device *pdev)
 	/* Initialize the I2C adapter */
 	i2c_bus->adap.owner = THIS_MODULE;
 	i2c_bus->adap.algo = &i2c_ast2600_algorithm;
+	i2c_bus->adap.quirks = &ast2600_i2c_quirks;
 	i2c_bus->adap.retries = 0;
 	i2c_bus->adap.dev.parent = i2c_bus->dev;
 	device_set_node(&i2c_bus->adap.dev, dev_fwnode(dev));
