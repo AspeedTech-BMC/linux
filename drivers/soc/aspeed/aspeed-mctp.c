f60022635ed04221d9e27c57feabf8e61fa0e2d3
YunHsuanChung <yh_chung@aspeedtech.com>
2026-06-01 19:51:20 +0800

    soc: aspeed: Fix aspeed-mctp BDF check for RC mode
    Previously, the aspeed-mctp driver regarded BDF 0 as valid both in EP
    and RC mode, which caused issues when the BMC acts as PCIE EP and tries
    to issue packet after link status is established but before the host
    assigns a BDF address.
    
    This commit updates the BDF check to allow BDF 0 only in RC mode,
    ensuring that the driver can function correctly in both RC and EP modes.
    
    Use "pcie_rc" compatible in DTS to indicate RC mode, priv->rc_f will be
    set accordingly in the driver. This allows the driver to determine
    whether it is operating in RC mode and apply the appropriate BDF check.
    
    Signed-off-by: YunHsuanChung <yh_chung@aspeedtech.com>
    Change-Id: I6457ef53ad1519ca70e589f8257c32462fb0ce4e
    (cherry picked from commit 9344f07e91da1518406c8dd3137465d6ca12a083)

--- drivers/soc/aspeed/aspeed-mctp.c
+++ drivers/soc/aspeed/aspeed-mctp.c
@@ -440,6 +440,9 @@ static int _get_bdf(struct aspeed_mctp *priv)
 		bdf = PCI_DEVID(GET_PCI_BUS_NUM(reg), devfn);
 	}
 
+	if (!priv->rc_f && bdf == 0)
+		return -ENODEV;
+
 	return bdf;
 }
 
@@ -2290,11 +2293,15 @@ static int aspeed_mctp_resources_init(struct aspeed_mctp *priv)
 		return PTR_ERR(priv->reset);
 	}
 
-	if (priv->rc_f) {
-		priv->reset_dma = devm_reset_control_get_shared_by_index(priv->dev, 1);
-		if (IS_ERR(priv->reset_dma)) {
-			dev_err(priv->dev, "Failed to get ep reset!\n");
-			return PTR_ERR(priv->reset_dma);
+	if (priv->match_data->dma_need_64bits_width) {
+		priv->reset_dma = NULL;
+	} else {
+		if (priv->rc_f) {
+			priv->reset_dma = devm_reset_control_get_shared_by_index(priv->dev, 1);
+			if (IS_ERR(priv->reset_dma)) {
+				dev_err(priv->dev, "Failed to get ep reset!\n");
+				return PTR_ERR(priv->reset_dma);
+			}
 		}
 	}
 	priv->pcie.map =
@@ -2455,7 +2462,7 @@ static int aspeed_mctp_hw_reset(struct aspeed_mctp *priv)
 		return ret;
 	}
 
-	if (priv->rc_f) {
+	if (priv->rc_f && priv->reset_dma) {
 		ret = reset_control_deassert(priv->reset_dma);
 		if (ret) {
 			dev_warn(priv->dev, "Failed to deassert ep reset\n");
